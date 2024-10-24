786bfb403fc05efd4da5dec35d692fde4fa0f02b
Sergio Jauregui <sergioja2003@gmail.com>
2024-10-23 21:34:58 -0500

    Project Retomed

--- server/src/config.js
+++ server/src/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 5173,
   /* Database */
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_DATABASE: process.env.DB_DATABASE || "pinup",
